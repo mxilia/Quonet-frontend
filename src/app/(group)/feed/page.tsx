157782e7ba3cfea95837487fe5cf8a4b535b802a
mxilia <ravintkosol@gmail.com>
2025-12-29 22:05:55 +0700

    Kim

--- src/app/(group)/feed/page.tsx
+++ src/app/(group)/feed/page.tsx
@@ -1,5 +1,6 @@
 import { PostsList } from "@/features/posts/components/posts-list";
 import { Metadata } from "next";
+import { Activity } from "react";
 
 export const metadata: Metadata = {
   title: "Feed",
@@ -10,7 +11,9 @@ export const metadata: Metadata = {
 const FeedPage = () => {
   return (
     <div className="bg-black min-h-screen pt-17 text-white sm:px-0 px-2 flex gap-2 justify-center">
-      <PostsList />
+      <Activity>
+        <PostsList />
+      </Activity>
     </div>
   )
 }
