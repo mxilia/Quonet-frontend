e699a8e334bd379b139f55b28382556e4e29c104
mxilia <ravintkosol@gmail.com>
2025-12-29 14:43:48 +0700

    Updated not-found page.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export const dynamic = 'force-dynamic';
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col gap-1 items-center justify-center bg-black text-white font-semibold">
+    <div className="flex flex-col h-screen gap-1 items-center justify-center bg-black text-white font-semibold">
       <h1>404 - Not Found</h1>
       <Link href={path.home.getHref()} replace> Go to Home</Link>
     </div>
