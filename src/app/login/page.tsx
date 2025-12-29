c49197e5edbb61c1424695683a109888fc7f8856
mxilia <ravintkosol@gmail.com>
2025-12-29 14:41:17 +0700

    Fixed login button.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,30 @@
-'use server';
+'use client';
 
 import { path } from "@/config/path";
 import { RedirectButton } from "./_components/redirect-button";
 import Image from "next/image";
 import { env } from "process";
-import { LoginButton } from "./_components/login-button";
+import { useEffect, useState } from "react";
+import { useUser } from "@/lib/auth";
 
 const LoginPage = () => {
   const loginPath = `${env.API_URL}/auth/google/login`;
-  
+
+  const user = useUser();
+  const  [redirectPath, setRedirect] = useState("");
+
+  useEffect(() => {
+    if(user.data) setRedirect(path.home.getHref())
+    else setRedirect(loginPath)
+  }, [user.data]);
+
   return (
     <div className="h-screen flex justify-center items-center bg-black">
       <div className="bg-(--darker-foreground) w-100 rounded-xl flex items-center flex-col p-2 gap-2 pt-4 pb-4">
         <div className="h-20 w-70 flex justify-center items-center">
           <Image src="/logo.svg" alt="quonet's logo" width={200} height={50} className="object-cover object-center" />
         </div>
-        <LoginButton text="Sign in with Google Account" loginRedirectPath={loginPath} defaultRedirectPath={path.home.getHref()} imgPath="/google-logo.png"/>
+        <RedirectButton text="Sign in with Google Account" redirectPath={redirectPath} imgPath="/google-logo.png"/>
         <div className="text-neutral-500 flex w-10/12 items-center">
           <div className="border-t w-full h-0"></div>
           <div className="ml-1 mr-1">OR</div>
