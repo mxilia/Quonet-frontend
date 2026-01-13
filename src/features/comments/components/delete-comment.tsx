e853d67fe8bad3cac882fb97ceb4783eb0e5da94
mxilia <ravintkosol@gmail.com>
2026-01-13 10:00:13 +0700

    Test #8

--- src/features/comments/components/delete-comment.tsx
+++ src/features/comments/components/delete-comment.tsx
@@ -12,38 +12,36 @@ type DeleteCommentProps = {
 
 export const DeleteComment = ({ comment }: DeleteCommentProps) => {
   const user = useUser()
-  const deleteComment = useDeleteComment()
+  const deleteComment = useDeleteComment({
+    mutationConfig: {
+      onSuccess: () => {
+        notify({
+          type: "success",
+          message: "Deleted comment successfully",
+        })
+      },
+      onError: (error, variables, onMutateResult, context) => {
+        notify({
+          type: "error",
+          message:
+            "Failed to delete comment " +
+            error.message +
+            ", " +
+            (onMutateResult || "null") +
+            ", " +
+            error.name +
+            ", " +
+            (error.cause || "null") +
+            ", " +
+            (error.stack || "null"),
+        })
+      },
+    },
+  })
   const notify = useNotificationStore((s) => s.notify)
 
   if (!canDeleteComment(user.data, comment)) return null
-  const onDelete = () =>
-    deleteComment.mutate(
-      { commentId: comment.id },
-      {
-        onSuccess: () => {
-          notify({
-            type: "success",
-            message: "Deleted comment successfully",
-          })
-        },
-        onError: (error, variables, onMutateResult, context) => {
-          notify({
-            type: "error",
-            message:
-              "Failed to delete comment " +
-              error.message +
-              ", " +
-              (onMutateResult || "null") +
-              ", " +
-              error.name +
-              ", " +
-              (error.cause || "null") +
-              ", " +
-              (error.stack || "null"),
-          })
-        },
-      },
-    )
+  const onDelete = () => deleteComment.mutate({ commentId: comment.id })
 
   return (
     <button className="text-xs text-red-500" onClick={onDelete}>
